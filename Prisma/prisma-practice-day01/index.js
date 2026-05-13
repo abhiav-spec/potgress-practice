95bf7ae4088acaad1834afc4e454b1f91f8b5a6e
abhiav-spec <kumarabhinav6649@gmail.com>
2026-05-14 01:22:00 +0530

    some data pushed using prisma client

---
+++ Prisma/prisma-practice-day01/index.js
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function createuser() {
+    const user = await prisma.user.create({
+        data: {
+            name: "John Doe",
+            email: "john2@example.com",
+            city: "New York",
+            password: "password",
+        },
+    });
+    console.log(user);
+}
+
+createuser()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
+
